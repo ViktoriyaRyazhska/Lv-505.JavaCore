8c948f30a27e1b778ae9314b8b1eeb011e5da302
MaxAndrusenko <60259820+MaxAndrusenko@users.noreply.github.com>
2020-05-06 19:12:51 +0300

    Create Task1.java

---
+++ Hw_1/Task1.java
@@ -0,0 +1,14 @@
+package MyFirstHomework;
+import java.util.Scanner;
+
+public class MyFirstHometask {
+
+  public static void main(String[] args) {
+    System.out.println("Enter R: ");
+    Scanner scan = new Scanner(System.in);
+    int R = scan.nextInt();
+        System.out.println("P = " + 2*R*3.14);
+        System.out.println("S = " + R*R);
+  }
+
+}
