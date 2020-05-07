9d249f65fec614e8cffeac3542a6365c7d56d276
alex13-kov <64859979+alex13-kov@users.noreply.github.com>
2020-05-07 21:19:27 +0300

    Update phone.java

--- home work 1/phone.java
+++ home work 1/phone.java
@@ -6,20 +6,20 @@ public class phone {
 	static Scanner sc = new Scanner(System.in);
 	   public static void main(String args[])
 	   {
-	      System.out.print("скількі хвелин розмовляли з Польшею (9,50 грт/хв)- ");
+	          System.out.print("СЃРєС–Р»СЊРєС– С…РІРµР»РёРЅ СЂРѕР·РјРѕРІР»СЏР»Рё Р· РџРѕР»СЊС€РµСЋ (9,50 РіСЂС‚/С…РІ)- ");
 	     	      double c1 = sc.nextDouble();
-	     	     System.out.print("скількі хвелин розмовляли з Китаем (18 грт/хв)- ");
+	     	     System.out.print("СЃРєС–Р»СЊРєС– С…РІРµР»РёРЅ СЂРѕР·РјРѕРІР»СЏР»Рё Р· РљРёС‚Р°РµРј (18 РіСЂС‚/С…РІ)- ");
 	     	      double c2 = sc.nextDouble();
-	     	     System.out.print("скількі хвелин розмовляли з КНДР (120,50 грт/хв)- ");
+	     	     System.out.print("СЃРєС–Р»СЊРєС– С…РІРµР»РёРЅ СЂРѕР·РјРѕРІР»СЏР»Рё Р· РљРќР”Р  (120,50 РіСЂС‚/С…РІ)- ");
 	     	      double c3 = sc.nextDouble();
 	      	      double t1 = (9.50 * c1);
-	      System.out.println("Ціна за розмову з Поляком = " + t1);
+	      System.out.println("Р¦С–РЅР° Р·Р° СЂРѕР·РјРѕРІСѓ Р· РџРѕР»СЏРєРѕРј = " + t1);
 	      double t2 = (18 * c2);
-	      System.out.println("Ціна за розмову з Китайцем = " + t2);
+	      System.out.println("Р¦С–РЅР° Р·Р° СЂРѕР·РјРѕРІСѓ Р· РљРёС‚Р°Р№С†РµРј = " + t2);
 	      double t3 = (120.50 * c3);
-	      System.out.println("Ціна за розмову з Повстанцем КНДР = " + t3);
+	      System.out.println("Р¦С–РЅР° Р·Р° СЂРѕР·РјРѕРІСѓ Р· РџРѕРІСЃС‚Р°РЅС†РµРј РљРќР”Р  = " + t3);
 	    	      double suma= (t1 + t2 + t3);
-	      System.out.println("Загальний борг становить = " + suma) ;
+	      System.out.println("Р—Р°РіР°Р»СЊРЅРёР№ Р±РѕСЂРі СЃС‚Р°РЅРѕРІРёС‚СЊ = " + suma) ;
 	   }
 	}
 
